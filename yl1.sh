d13c8ef2662cb2da248098893705a638ef29371f
jaanikah <jaanika.haki@vikk.ee>
2024-03-26 14:07:33 +0200

    lisatud kommentarid

--- yl1.sh
+++ yl1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#yl1
+#yl1, tegi Jaanika Haki, ISK23, 26.03.2024
 
-echo "Tere tulemast Bash skriptimise kursusele!"
-echo
-echo "Täna on:"
-formatted_date=$(date +"%a %b %d %T %Z %Y")
-echo  "$formatted_date"
-echo
-echo "See on hea päev Bash skriptimise alustamiseks!"
+echo "Tere tulemast Bash skriptimise kursusele!" #lisan teksti "" vaheel
+echo  # tühi rida
+echo "Täna on:"  #tekstirida, sissejuhatus järgneva rea kuupäevale:
+formatted_date=$(date +"%a %b %d %T %Z %Y")  #kuupäev - nädalapäev, kuu, kuupäev, kell (t-m-s), Ida-Euroopa aeg, aasta
+echo  "$formatted_date"  # käivitan "formatted_date" rea
+echo # tühi rida
+echo "See on hea päev Bash skriptimise alustamiseks!" # viimane rida teksti
 
+#30 4 * * *  /home/student/skriptid/yl1.sh  crontab'i ajastamine
