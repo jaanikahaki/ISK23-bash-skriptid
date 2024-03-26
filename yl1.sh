ab282a433d7e07a4c3d1515ff28fdd7fd23e49c5
jaanikah <jaanika.haki@vikk.ee>
2024-03-26 13:50:32 +0200

    Ylesanne 1 Bash skriptimise ainest

---
+++ yl1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#yl1
+
+echo "Tere tulemast Bash skriptimise kursusele!"
+echo
+echo "Täna on:"
+formatted_date=$(date +"%a %b %d %T %Z %Y")
+echo  "$formatted_date"
+echo
+echo "See on hea päev Bash skriptimise alustamiseks!"
+
